b4d5c46f1442f3a07810521efecc227ed3fe5f60
trankhacduy91 <39598800+trankhacduy91@users.noreply.github.com>
2020-06-09 22:32:19 +0700

    add validation Contacts
    add validation Contacts_DuyTK

--- Model/EF/Contact.cs
+++ Model/EF/Contact.cs
@@ -12,15 +12,24 @@
         public int ID { get; set; }
 
         [StringLength(50)]
+        [Required(ErrorMessage ="Please input Name.")]
+        [Range(3,24)]
         public string Name { get; set; }
 
         [StringLength(150)]
+        [Required(ErrorMessage = "Please input Email.")]
+        [DataType(DataType.EmailAddress)]
+        [Range(3, 50)]
         public string Email { get; set; }
 
         [StringLength(50)]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Please input Phone.")]
         public string Phone { get; set; }
 
         [StringLength(250)]
+        [Required(ErrorMessage = "Please input Message.")]
+        [Range(10, 250)]
         public string Message { get; set; }
 
 
